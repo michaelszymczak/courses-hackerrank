0a4fd743d7a7646c04281429512f0037bee782e9
Michael Szymczak <contact@michaelszymczak.com>
2018-06-24 15:07:11 +0100

    Codility: prepare for the lesson002

---
+++ src/main/java/com/michaelszymczak/courses/codility/lessons/lesson002/Solution.java
@@ -0,0 +1,13 @@
+package com.michaelszymczak.courses.codility.lessons.lesson002;
+
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+  public int[] solution(int[] A, int K) {
+    return null;
+  }
+}
---
+++ src/test/java/com/michaelszymczak/courses/codility/lessons/lesson002/SolutionTest.java
@@ -0,0 +1,10 @@
+package com.michaelszymczak.courses.codility.lessons.lesson002;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created 24/06/18.
+ */
+public class SolutionTest {
+
+}
