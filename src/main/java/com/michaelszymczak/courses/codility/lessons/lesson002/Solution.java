10ba7e1e5c011e0398c2abebf6d66ab710be834e
Michael Szymczak <contact@michaelszymczak.com>
2018-06-24 15:41:19 +0100

    Codility: Lesson002 done, 100%

--- src/main/java/com/michaelszymczak/courses/codility/lessons/lesson002/Solution.java
+++ src/main/java/com/michaelszymczak/courses/codility/lessons/lesson002/Solution.java
@@ -6,8 +6,45 @@ package com.michaelszymczak.courses.codility.lessons.lesson002;
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
 
+import static java.lang.System.arraycopy;
+
 class Solution {
   public int[] solution(int[] A, int K) {
-    return null;
+    return new Rotation(A.length, K).rotated(A);
+  }
+
+  private static class Rotation {
+
+    final int sizeOfTheWholeArray;
+    final int numberOfElementsMovedToStart;
+    final int numberOfRemainingElements;
+    final int positionOfTheFirstElementThatWillBeMovedToStart;
+
+    public Rotation(int sizeOfTheWholeArray, int rotation) {
+      this.sizeOfTheWholeArray = sizeOfTheWholeArray;
+      this.numberOfElementsMovedToStart = sizeOfTheWholeArray > 0 ? rotation % sizeOfTheWholeArray : 0;
+      this.numberOfRemainingElements = sizeOfTheWholeArray - numberOfElementsMovedToStart;
+      this.positionOfTheFirstElementThatWillBeMovedToStart = sizeOfTheWholeArray - numberOfElementsMovedToStart;
+    }
+
+    public int[] rotated(int[] input) {
+      if (input.length != sizeOfTheWholeArray) {
+        throw new IllegalArgumentException("Invalid input array length");
+      }
+
+      final int[] result = new int[sizeOfTheWholeArray];
+      copyRotated(input, result);
+      copyRemaining(input, result);
+
+      return result;
+    }
+
+    private void copyRemaining(int[] input, int[] result) {
+      arraycopy(input, 0, result, numberOfElementsMovedToStart, numberOfRemainingElements);
+    }
+
+    private void copyRotated(int[] input, int[] result) {
+      arraycopy(input, positionOfTheFirstElementThatWillBeMovedToStart, result, 0, numberOfElementsMovedToStart);
+    }
   }
 }
--- src/test/java/com/michaelszymczak/courses/codility/lessons/lesson002/SolutionTest.java
+++ src/test/java/com/michaelszymczak/courses/codility/lessons/lesson002/SolutionTest.java
@@ -1,5 +1,7 @@
 package com.michaelszymczak.courses.codility.lessons.lesson002;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -7,4 +9,34 @@ import static org.junit.Assert.*;
  */
 public class SolutionTest {
 
+  private Solution solution = new Solution();
+
+  @Test
+  public void shouldReturnTheSameArrayIfNoRotation() throws Exception {
+    assertArrayEquals(new int[]{1,2,3}, solution.solution(new int[]{1,2,3}, 0));
+  }
+
+  @Test
+  public void shouldPlaceElementInTheBeginningIfRotated() throws Exception {
+    assertArrayEquals(new int[]{3,1,2}, solution.solution(new int[]{1,2,3}, 1));
+    assertArrayEquals(new int[]{2,3,1}, solution.solution(new int[]{1,2,3}, 2));
+  }
+
+  @Test
+  public void shouldPlReturnIdenticalArrayIfRotatedAll() throws Exception {
+    assertArrayEquals(new int[]{1,2,3}, solution.solution(new int[]{1,2,3}, 3));
+    assertArrayEquals(new int[]{1,2,3}, solution.solution(new int[]{1,2,3}, 6));
+  }
+
+  @Test
+  public void shouldAllowRotationsLargerThanSizeOfTheArray() throws Exception {
+    assertArrayEquals(solution.solution(new int[]{1,2,3}, 1), solution.solution(new int[]{1,2,3}, 4));
+    assertArrayEquals(solution.solution(new int[]{1,2,3}, 1), solution.solution(new int[]{1,2,3}, 7));
+  }
+
+  @Test
+  public void shouldHandleEmptyArray() throws Exception {
+    assertArrayEquals(new int[]{}, solution.solution(new int[]{}, 0));
+    assertArrayEquals(new int[]{}, solution.solution(new int[]{}, 5));
+  }
 }
